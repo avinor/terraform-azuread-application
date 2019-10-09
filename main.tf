3746c1a50656b242505ca7668de866d1361d679f
Terje Torkelsen <terje.torkelsen@avinor.no>
2019-10-09 17:43:56 +0200

    Created more advanced setup

--- main.tf
+++ main.tf
@@ -7,10 +7,35 @@ terraform {
 
 resource "azuread_application" "main" {
   name                       = var.name
+  homepage                   = var.homepage
   identifier_uris            = var.identifier_uris
   reply_urls                 = var.reply_urls
   available_to_other_tenants = false
-  oauth2_allow_implicit_flow = false
+  oauth2_allow_implicit_flow = var.oauth2_allow_implicit_flow
+  type                       = var.type
+  group_membership_claims    = var.group_membership_claims
+
+  dynamic "required_resource_access" {
+    for_each = var.required_resource_access
+    iterator = resource
+    content {
+      resource_app_id = resource.value.resource_app_id
+
+      dynamic "resource_access" {
+        for_each = resource.value.resource_access
+        iterator = access
+        content {
+          id   = access.value.id
+          type = access.value.type
+        }
+      }
+    }
+  }
+}
+
+resource "azuread_service_principal" "main" {
+  application_id               = azuread_application.main.application_id
+  app_role_assignment_required = false
 }
 
 resource "random_string" "unique" {
--- variables.tf
+++ variables.tf
@@ -2,16 +2,50 @@ variable "name" {
   description = "Name of the application."
 }
 
+variable "homepage" {
+  description = "The URL to the application's home page. If no homepage is specified this defaults to `https://{name}`"
+  default     = null
+}
+
 variable "identifier_uris" {
-  description = "Identifies uris for application."
+  description = "A list of user-defined URI(s) that uniquely identify a Web application within it's Azure AD tenant, or within a verified custom domain if the application is multi-tenant."
   type        = list(string)
   default     = []
 }
 
 variable "reply_urls" {
-  description = "Reply urls for application."
+  description = "A list of URLs that user tokens are sent to for sign in, or the redirect URIs that OAuth 2.0 authorization codes and access tokens are sent to."
   type        = list(string)
   default     = []
+}
+
+variable "oauth2_allow_implicit_flow" {
+  description = "Does this Azure AD Application allow OAuth2.0 implicit flow tokens?"
+  type        = bool
+  default     = false
+}
+
+variable "type" {
+  description = "Type of an application: `webapp/api` or `native`."
+  default     = "webapp/api"
+}
+
+variable "required_resource_access" {
+  description = "Required resource access for this application."
+  type = list(
+    object({
+      resource_app_id = string,
+      resource_access = list(
+        object({
+          id   = string,
+          type = string
+      }))
+  }))
+}
+
+variable "group_membership_claims" {
+  description = "Configures the groups claim issued in a user or OAuth 2.0 access token that the app expects. Possible values are `None`, `SecurityGroup` or `All`."
+  default     = "SecurityGroup"
 }
 
 variable "end_date" {
